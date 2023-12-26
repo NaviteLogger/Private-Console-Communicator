58bd07818c991f23b2e6bab83d815a0913afb919
Marek Kacprzak <kacprzakmarek92@gmail.com>
2023-12-26 17:29:16 +0100

    Refactor generate_key_pair_for_client function

--- server/server_functions.py
+++ server/server_functions.py
@@ -5,6 +5,5 @@ from cryptography.hazmat.backends import default_backend
 def generate_key_pair_for_client():
     # Generate an RSA key pair for the desired client
     private_key = rsa.generate_private_key(
-        public_exponent=65537,
-        key_size=2048,
+        public_exponent=65537, key_size=2048, backend=default_backend()
     )
