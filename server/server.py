c839d0efbdc60df132cc5d953b492b571d86dfa9
Marek Kacprzak <kacprzakmarek92@gmail.com>
2023-12-26 22:56:13 +0100

    Remove database connection code

--- server/server.py
+++ server/server.py
@@ -1,8 +1,6 @@
 import server_functions
 import socket
 
-# Establish a connectio to the database
-connection = server_functions.connect()
 
 # Create a socket to listen for incoming connections
 server_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
