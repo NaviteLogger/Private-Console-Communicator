53c07dd1b59f80491432bafebb40acd6f1f1855b
Marek Kacprzak <kacprzakmarek92@gmail.com>
2023-12-27 00:11:51 +0100

    Refactor code to move client functions to a separate module

--- client/app.py
+++ client/app.py
@@ -1,6 +1,1 @@
-import socket
-import threading
-from cryptography.hazmat.backends import default_backend
-from cryptography.hazmat.primitives import serialization, hashes
-from cryptography.hazmat.primitives.asymmetric import rsa, padding
-
+import client_functions
---
+++ client/client_functions.py
@@ -0,0 +1,5 @@
+import socket
+import threading
+from cryptography.hazmat.backends import default_backend
+from cryptography.hazmat.primitives import serialization, hashes
+from cryptography.hazmat.primitives.asymmetric import rsa, padding
