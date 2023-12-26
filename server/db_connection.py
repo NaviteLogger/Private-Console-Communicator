37812fe178279025081151266de4f1460cdf04b0
Marek Kacprzak <kacprzakmarek92@gmail.com>
2023-12-26 17:03:26 +0100

    Update PostgreSQL database connection credentials

--- server/db_connection.py
+++ server/db_connection.py
@@ -1,13 +1,17 @@
 # This file will be solely responsible for connecting to the PostgreSQL database.
-import psycopg2
+import psycopg2, os
+from dotenv import load_dotenv
+
+# Get the environment variables
+load_dotenv()
 
 
 # Connect to the PostgreSQL database
 def connect():
     try:
         connection = psycopg2.connect(
-            user="postgres",
-            password="postgres",
+            user=os.getenv("POSTGRESQL_DB_USER"),
+            password=os.getenv("POSTGRESQL_DB_PASSWORD"),
             host="localhost",
             port="5432",
             database="postgres",
