521dd47bd076516fe4f3b35b75d3d20a2fbc5a88
Marek Kacprzak <kacprzakmarek92@gmail.com>
2023-12-27 00:13:45 +0100

    Add send_message function to client_functions.py

--- client/client_functions.py
+++ client/client_functions.py
@@ -3,3 +3,11 @@ import threading
 from cryptography.hazmat.backends import default_backend
 from cryptography.hazmat.primitives import serialization, hashes
 from cryptography.hazmat.primitives.asymmetric import rsa, padding
+
+
+def send_message(sock, message, recipient_public_key):
+    # Encrypt the message to be sent with the server's provi
+    encrypted_message = recipient_public_key.encrypt(message, padding.OAEP(mgf=padding.MGF1(algorithm=hashes.SHA256()), algorithm=hashes.SHA256(), label=None))
+
+    # Send the encrypted message to the server
+    sock.send(encrypted_message)
