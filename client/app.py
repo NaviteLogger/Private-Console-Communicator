6b24fb8903f49ca0ddf9b0756b02f5ead95b76aa
Marek Kacprzak <kacprzakmarek92@gmail.com>
2023-12-27 00:11:31 +0100

    Added the 'client_functions.py' file

--- client/app.py
+++ client/app.py
@@ -3,3 +3,4 @@ import threading
 from cryptography.hazmat.backends import default_backend
 from cryptography.hazmat.primitives import serialization, hashes
 from cryptography.hazmat.primitives.asymmetric import rsa, padding
+
