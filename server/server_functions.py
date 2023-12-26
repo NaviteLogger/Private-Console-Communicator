3d221146500b9410e5a7cfbdc636735154e4fd2b
Marek Kacprzak <kacprzakmarek92@gmail.com>
2023-12-26 17:29:10 +0100

    Import rsa and generate RSA key pair for client

--- server/server_functions.py
+++ server/server_functions.py
@@ -1,5 +1,10 @@
-from 
+from cryptography.hazmat.primitives.asymmetric import rsa
+from cryptography.hazmat.backends import default_backend
+
 
 def generate_key_pair_for_client():
     # Generate an RSA key pair for the desired client
-    private_key = 
+    private_key = rsa.generate_private_key(
+        public_exponent=65537,
+        key_size=2048,
+    )
