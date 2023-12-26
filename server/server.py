e0f085208bda6181998e5e22074c13f034ed21bb
Marek Kacprzak <kacprzakmarek92@gmail.com>
2023-12-26 22:53:28 +0100

    Refactor database connection and import server functions

--- server/server.py
+++ server/server.py
@@ -1,8 +1,8 @@
-from db_connection import connect
+import server_functions
 import socket
 
 # Establish a connectio to the database
-connection = connect()
+connection = server_functions.connect()
 
 # Create a socket to listen for incoming connections
 server_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
