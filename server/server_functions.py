4a7f75d1a8ed95bae2139d28daf1f63b2df7d3a8
Marek Kacprzak <kacprzakmarek92@gmail.com>
2023-12-26 17:30:13 +0100

    Add serialization of public key for client

--- server/server_functions.py
+++ server/server_functions.py
@@ -1,5 +1,7 @@
-from cryptography.hazmat.primitives.asymmetric import rsa
 from cryptography.hazmat.backends import default_backend
+from cryptography.hazmat.primitives.asymmetric import rsa
+from cryptography.hazmat.primitives import serialization, hashes
+from cryptography.hazmat.primitives.asymmetric import padding
 
 
 def generate_key_pair_for_client():
@@ -7,3 +9,6 @@ def generate_key_pair_for_client():
     private_key = rsa.generate_private_key(
         public_exponent=65537, key_size=2048, backend=default_backend()
     )
+
+    # Serialize the public key to send to the client
+    serialized_public_key = public
