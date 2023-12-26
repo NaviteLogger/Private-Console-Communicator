f94a34e48b4f5b3c8f344cbfa9b6bf34a7bca2ba
Marek Kacprzak <kacprzakmarek92@gmail.com>
2023-12-26 17:25:44 +0100

    Add socket functionality to server.py

--- server/server.py
+++ server/server.py
@@ -4,3 +4,7 @@ import socket
 # Establish a connectio to the database
 connection = connect()
 
+# Create a socket to listen for incoming connections
+server_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+server_socket.bind((socket.gethostname(), 5555))
+server_socket.listen(5)
