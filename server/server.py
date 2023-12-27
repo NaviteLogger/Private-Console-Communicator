8de9e57019d5e7a8f40e1201ca0e309af043cf7f
Marek Kacprzak <kacprzakmarek92@gmail.com>
2023-12-27 14:48:28 +0100

    Removed two sql database tables declarations

--- server/server.py
+++ server/server.py
@@ -12,9 +12,10 @@ cursor.execute(
     """
     CREATE TABLE IF NOT EXISTS connections (
         id INTEGER PRIMARY KEY,
+        public_key TEXT,
         address TEXT,
         port INTEGER,
-        char_room_id TEXT,
+
     )
 """
 )
@@ -30,23 +31,7 @@ cursor.execute(
     )"""
 )
 
-cursor.execute(
-    """
-    CREATE TABLE IF NOT EXISTS chat_rooms (
-        id INTEGER PRIMARY KEY,
-        chat_room_id TEXT,
-    )"""
-)
 
-cursor.execute(
-    """
-    CREATE TABLE IF NOT EXISTS user_chat_rooms (
-        user_id INTEGER,
-        chat_room_id TEXT,
-        FOREIGN KEY (user_id) REFERENCES connections (id),
-        FOREIGN KEY (chat_room_id) REFERENCES chat_rooms (id),
-        )"""
-)
 
 # Commit the changes to the database
 connection.commit()
