a5b4fa0bace25349428a9fbfa98ee1707e823f8c
Marek Kacprzak <kacprzakmarek92@gmail.com>
2023-12-27 17:51:53 +0100

    Refactor client_functions.py: Add read_config function

--- client/client_functions.py
+++ client/client_functions.py
@@ -1,8 +1,21 @@
 import socket
+import configparser
 import threading
 from cryptography.hazmat.backends import default_backend
 from cryptography.hazmat.primitives import serialization, hashes
 from cryptography.hazmat.primitives.asymmetric import rsa, padding
+
+
+def read_config():
+    # Read the client configuration file
+    config = configparser.ConfigParser()
+
+def connect_to_server():
+    # Create a socket to connect to the server
+    sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    sock.connect((socket.gethostname(), 5555))
+
+    return sock
 
 
 def send_message(sock, message, recipient_public_key):
@@ -11,11 +24,3 @@ def send_message(sock, message, recipient_public_key):
 
     # Send the encrypted message to the server
     sock.send(encrypted_message)
-
-
-def connect_to_server():
-    # Create a socket to connect to the server
-    sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-    sock.connect((socket.gethostname(), 5555))
-
-    return sock
