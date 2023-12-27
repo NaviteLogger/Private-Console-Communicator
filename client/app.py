74d69a7e30b360ee0c05f30c576cc77390b8f236
Marek Kacprzak <kacprzakmarek92@gmail.com>
2023-12-27 17:56:52 +0100

    Remove redundant code in app.py

--- client/app.py
+++ client/app.py
@@ -3,5 +3,4 @@ import client_functions
 # Connect to the server
 sock = client_functions.connect_to_server()
 
-if __name__ == "__main__":
-    client_socket = client_functions.connect_to_server()
+
