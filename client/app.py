e0bac0299f6c16e3308d2c08ed7fc24b71113c3a
Marek Kacprzak <kacprzakmarek92@gmail.com>
2023-12-27 17:58:27 +0100

    Refactor client/app.py: Connect to server in main function

--- client/app.py
+++ client/app.py
@@ -1,6 +1,4 @@
 import client_functions
 
-# Connect to the server
-sock = client_functions.connect_to_server()
-
-
+if __name__ == "__main__":
+    client_socket = client_functions.connect_to_server()
