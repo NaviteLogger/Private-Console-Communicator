50a4ebd792af821f99b62c3390c7b3fcdff63c4d
Marek Kacprzak <kacprzakmarek92@gmail.com>
2023-12-28 17:01:47 +0100

    Update README.md and app.py

--- client/app.py
+++ client/app.py
@@ -4,3 +4,7 @@ if __name__ == "__main__":
     client_socket = client_functions.connect_to_server()
 
     print("Connected to the server.")
+
+    while True:
+        # After establishing a connection with the server, prompt the user with a menu
+        print("Welcome to the Private Console Communicator!")
