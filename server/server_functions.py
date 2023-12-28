9b0959d2bc2da21918a07e50aec83bfe415c4e73
Marek Kacprzak <kacprzakmarek92@gmail.com>
2023-12-28 10:45:34 +0100

    Refactoring the code

--- server/server_functions.py
+++ server/server_functions.py
@@ -12,7 +12,7 @@ load_dotenv()
 # Connect to the SQLite database
 def connect_to_database():
     # Connect to the SQLite database
-    connection = sqlite3.connect()
+    connection = sqlite3.connect(os.path.join("database", "database.db"))
     cursor = connection.cursor()
 
     return cursor, connection
